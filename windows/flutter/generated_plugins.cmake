6cfbe7b315d22a29f47e918397cfa517584a9203
AnyumbaKE <esanyumba@gmail.com>
2024-07-21 22:28:51 +0300

    dependancies update

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,9 +3,13 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  file_selector_windows
+  permission_handler_windows
+  url_launcher_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
+  flutter_zxing
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
