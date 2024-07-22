2f2f069115531a331b89dbb1b0ece45eab0abe20
AnyumbaKE <esanyumba@gmail.com>
2024-07-22 14:55:18 +0300

    domain

---
+++ lib/domain.dart
@@ -0,0 +1,8 @@
+/// Domain of nutrition objects and location of other dataclasses,
+/// like data-carriers.
+library domain;
+
+export 'package:diet_planner/domain/nutrtion_app_domain.dart';
+export 'package:diet_planner/domain/data_carriers.dart';
+export 'package:diet_planner/domain/meal_component_factory.dart';
+export 'package:diet_planner/domain/enums.dart';
