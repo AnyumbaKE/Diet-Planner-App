cfe94e87b5e0fac76893bd51fff21111695ce5e2
AnyumbaKE <esanyumba@gmail.com>
2024-07-22 15:07:19 +0300

    domain/

--- lib/domain.dart
+++ lib/domain.dart
@@ -2,7 +2,7 @@
 /// like data-carriers.
 library domain;
 
-export 'package:diet_planner/domain/nutrtion_app_domain.dart';
+export 'package:diet_planner/domain/diet_planner_domain.dart';
 export 'package:diet_planner/domain/data_carriers.dart';
 export 'package:diet_planner/domain/meal_component_factory.dart';
 export 'package:diet_planner/domain/enums.dart';
