4405338cd806fe45fcbe6977578a445cd579f675
AnyumbaKE <esanyumba@gmail.com>
2024-07-23 10:04:44 +0300

    utils

---
+++ lib/utils.dart
@@ -0,0 +1,10 @@
+/// Util Functions for the project within and outside of the scope of the project
+/// like utility functions, utility widgets, general project widgets, and other general
+/// project functions like storage
+
+library util;
+
+export 'package:diet_planner/utils/utils.dart';
+export 'package:diet_planner/utils/local_widgets.dart';
+export 'package:diet_planner/utils/storage.dart';
+export 'package:diet_planner/utils/shared_functionalities.dart';
